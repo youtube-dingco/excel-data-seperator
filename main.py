fc804975a098843a1e7e54067ce06980af403157
hwangsawon <coding.hwangsawon@gmail.com>
2023-11-21 11:20:12 +0900

    xlsx to dataframe
    Signed-off-by: hwangsawon <coding.hwangsawon@gmail.com>

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-import utils
+import os
+import openpyxl
+import pandas as pd
+from itertools import islice
 
-file_names = utils.get_filenames_in(path="./", file_extension="xlsx")
-print(file_names)
+def get_filenames_in(path, file_extension=None):
+    filenames = os.listdir(path)
+    if file_extension:
+        filenames = [filename for filename in filenames if filename.endswith(file_extension)]
+    return filenames
+
+def xlsx_to_dataframe(path):
+    wb = openpyxl.load_workbook(filename)
+    ws = wb.worksheets[0]
+    data = ws.values
+    cols = next(data) # 첫 번째 행을 cols로 생각
+    data = [d for d in data]
+    return pd.DataFrame(data, columns=cols)
+
+file_names = get_filenames_in(path="./", file_extension="xlsx")
+filename = file_names[0]
+df = xlsx_to_dataframe(path=filename)
+
+print(df)
+print(df.columns)
+print(df.dtypes)
--- utils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def get_filenames_in(path, file_extension=None):
-    filenames = os.listdir(path)
-    if file_extension:
-        filenames = [filename for filename in filenames if filename.endswith(file_extension)]
-    return filenames
-    
